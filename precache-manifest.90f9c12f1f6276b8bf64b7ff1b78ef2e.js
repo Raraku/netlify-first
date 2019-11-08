c9cb85941adf27a793150b94006c7eb90946c463
Raraku <airenov500@gmail.com>
2019-11-08 10:27:27 +0100

    first release

---
+++ precache-manifest.90f9c12f1f6276b8bf64b7ff1b78ef2e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31eb18821f7366aed43450cd6a4c1dce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b38e267b6dfe45bfe15c",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "b4af9af4b425abf2287f",
+    "url": "/static/css/main.4bd4a753.chunk.css"
+  },
+  {
+    "revision": "b38e267b6dfe45bfe15c",
+    "url": "/static/js/2.149044f8.chunk.js"
+  },
+  {
+    "revision": "b4af9af4b425abf2287f",
+    "url": "/static/js/main.c7abded7.chunk.js"
+  },
+  {
+    "revision": "ade8718b60fc2c95101d",
+    "url": "/static/js/runtime-main.9eea7677.js"
+  },
+  {
+    "revision": "afa960aeb1c5be5db0829befe5ddb8f6",
+    "url": "/static/media/Martz90-Circle-Viber.afa960ae.ico"
+  },
+  {
+    "revision": "c9cdfc8a38314deddc0c40ffd59f8a0b",
+    "url": "/static/media/Twitter_Android.c9cdfc8a.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
